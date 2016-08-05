677622f19476efedc3c1c54e5aae6ae1f0bf13d3
Nathan Zimmerman <npzimmerman@gmail.com>
2016-08-05 15:27:02 -0400

    Use geowave entrypoint and ignore SIGHUP in iterator registration daemon

---
+++ fs/sbin/geowave-entrypoint.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+source /sbin/accumulo-lib.sh
+
+# The first argument determines this container's role in the accumulo cluster
+ROLE=${1:-}
+ACCUMULO_USER=${ACCUMULO_USER:-root}
+
+if [ $ROLE = "register" ]; then
+  wait_until_hdfs_is_available
+  with_backoff accumulo_is_available
+  accumulo shell -u ${ACCUMULO_USER} -p ${ACCUMULO_PASSWORD} -e \
+    "createnamespace geowave"
+  accumulo shell -u ${ACCUMULO_USER} -p ${ACCUMULO_PASSWORD} -e \
+    "config -s general.vfs.context.classpath.geowave=file:///usr/local/geowave/accumulo/[^.].*.jar"
+  accumulo shell -u ${ACCUMULO_USER} -p ${ACCUMULO_PASSWORD} -e \
+    "config -ns geowave -s table.classpath.context=geowave"
+  echo "Accumulo namespace configured: geowave"
+elif [ $ROLE = "master" ]; then
+  (setsid /sbin/geowave-entrypoint.sh register &> /tmp/geowave-register.log &)
+  /sbin/entrypoint.sh "$@"
+else
+  /sbin/entrypoint.sh "$@"
+fi
