4e09fa74531282835eb28bf0336b7775a7a3252b
James McClain <jmcclain@azavea.com>
2016-08-16 17:06:32 -0400

    Reduce Verbosity

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+echo "$(find /root/.m2 | grep jar$ | wc -l) jars found"
+
 cd /geowave
-mvn clean
-mvn install $EXTRA_ARGS $BUILD_ARGS
-mvn package -P accumulo-container-singlejar $EXTRA_ARGS $BUILD_ARGS
-mvn package -P geotools-container-singlejar $EXTRA_ARGS $BUILD_ARGS
-mvn package -P geowave-analytic-mapreduce $EXTRA_ARGS $BUILD_ARGS
-mvn package -P geowave-tools-singlejar $EXTRA_ARGS $BUILD_ARGS
+
+set -x
+mvn clean > /dev/null 2>&1
+mvn install $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+mvn package -P accumulo-container-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+mvn package -P geotools-container-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+mvn package -P geowave-analytic-mapreduce $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+mvn package -P geowave-tools-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+set +x
+
 chown -R $1:$2 /root/.m2
 chown -R $1:$2 /geowave
